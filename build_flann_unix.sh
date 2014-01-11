4b9c7475fbfc5bb9910beeb8470d9b3b90ea1c2b
Jon Crall <crallj@rpi.edu>
2014-01-10 20:04:53 -0500

    wip

--- build_flann_unix.sh
+++ build_flann_unix.sh
@@ -2,7 +2,10 @@ cd ~/code/flann
 mkdir build
 cd build
 
-cmake -G "Unix Makefiles" ..
+#sudo apt-get install libcr-dev mpich2 mpich2-doc
+
+cmake -G "Unix Makefiles" -DBUILD_MATLAB_BINDINGS=Off -DCMAKE_BUILD_TYPE=Release
+..
 
 make
 sudo make install
