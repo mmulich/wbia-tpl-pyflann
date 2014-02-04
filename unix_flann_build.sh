8b6870426b1cfce297205bc5ff16481ee6d93262
Jon Crall <crallj@rpi.edu>
2014-02-04 12:59:37 -0500

    Added logic to grab correct python executable

--- unix_flann_build.sh
+++ unix_flann_build.sh
@@ -1,11 +1,14 @@
 cd ~/code/flann
 mkdir build
-cd build
+cd ~/code/flann/build
 
 #sudo apt-get install libcr-dev mpich2 mpich2-doc
 
-cmake -G "Unix Makefiles" -DBUILD_MATLAB_BINDINGS=Off -DCMAKE_BUILD_TYPE=Release
-..
+# Grab correct python executable
+export PYTHON_EXECUTABLE=$(which python)
 
-make
-sudo make install
+cmake -G "Unix Makefiles" \
+    -DBUILD_MATLAB_BINDINGS=Off \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DPYTHON_EXECUTABLE=$PYTHON_EXECUTABLE \
+    .. && make && sudo make install
