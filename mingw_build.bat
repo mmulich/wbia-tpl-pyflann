c78af47302241ba4bd7739eda2fa9dac11734220
joncrall <crallj@rpi.edu>
2014-05-31 22:47:07 -0400

    OpenMP is working with mingw

--- mingw_build.bat
+++ mingw_build.bat
@@ -9,12 +9,12 @@ set INSTALL32=C:\Program Files (x86)
 set FLANN_INSTALL="%INSTALL32%\Flann"
 set CMAKE_EXE="%INSTALL32%\CMake 2.8\bin\cmake.exe"
 set CMAKE_GUI_EXE="%INSTALL32%\CMake 2.8\bin\cmake-gui.exe"
+set FLANN_DIR=%HOME%\code\flann
 
-
-cd %HOME%\code\flann
-:: rm -rf %code%\flann\build
-mkdir %HOME%\code\flann\build
-cd %HOME%\code\flann\build
+cd %FLANN_DIR%
+:: rm -rf build
+mkdir build
+cd build
 
 :: OpenCV settings on windows
 %CMAKE_EXE% -G "MSYS Makefiles" ^
@@ -23,10 +23,11 @@ cd %HOME%\code\flann\build
 -DCMAKE_BUILD_TYPE=Release ^
 -DCMAKE_C_FLAGS=-m32 ^
 -DCMAKE_CXX_FLAGS=-m32 ^
--DUSE_OPENMP=Off ^
+-DUSE_OPENMP=On ^
+-DUSE_MPI=Off ^
 -DHDF5_INCLUDE_DIRS="" ^
 -DHDF5_ROOT_DIR="" ^
- %code%\flann
+%FLANN_DIR%
 
 :: make command that doesn't freeze on mingw
 echo "BUILDING FLANN TAKES AWHILE. BE PATIENT."
