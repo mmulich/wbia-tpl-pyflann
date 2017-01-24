778d520aa9af313365590575e222fa1d8ed182da
joncrall <crallj@rpi.edu>
2017-01-23 20:01:13 -0500

    wip

--- src/python/setup.py.tpl
+++ src/python/setup.py.tpl
@@ -9,18 +9,24 @@ from os.path import exists, abspath, dirname, join
 import os
 import sys
 
+POSSIBLE_LIBS = ['libflann.so', 'flann.dll', 'libflann.dll', 'libflann.dylib']
+
 
 def find_path():
-    lib_paths = [ os.path.abspath('@LIBRARY_OUTPUT_PATH@'), abspath(join(dirname(dirname(sys.argv[0])), '../../../lib')) ]
-    possible_libs = ['libflann.so', 'flann.dll', 'libflann.dll', 'libflann.dylib']
+    lib_paths = [
+        # os.path.abspath('@LIBRARY_OUTPUT_PATH@'),
+        os.path.abspath('@LIBRARY_OUTPUT_DIRECTORY@'),
+        abspath(join(dirname(dirname(sys.argv[0])), '../../../lib'))
+    ]
 
     for path in lib_paths:
-        for lib in possible_libs:
+        for lib in POSSIBLE_LIBS:
             if exists(join(path,lib)):
                 return path
 
 setup(name='flann',
-      version='@FLANN_VERSION@',
+      # version='@FLANN_VERSION@',
+      version='1.8.4',
       description='Fast Library for Approximate Nearest Neighbors',
       author='Marius Muja',
       author_email='mariusm@cs.ubc.ca',
@@ -28,5 +34,5 @@ setup(name='flann',
       url='http://www.cs.ubc.ca/~mariusm/flann/',
       packages=['pyflann', 'pyflann.lib'],
       package_dir={'pyflann.lib': find_path() },
-      package_data={'pyflann.lib': ['libflann.so', 'flann.dll', 'libflann.dll', 'libflann.dylib']}, 
+      package_data={'pyflann.lib': POSSIBLE_LIBS}, 
 )
--- unix_build.sh
+++ unix_build.sh
@@ -1,27 +1,10 @@
 #!/bin/bash
-#REPODIR=$(cd $(dirname $0) ; pwd)
-#cd $REPODIR/flann
-
-#rm -rf build
 export ORIGDIR=$(pwd)
 export FLANNDIR=$ORIGDIR
 
-python2.7 -c "import utool as ut; print('keeping build dir' if not ut.get_argflag('--rmbuild') else ut.delete('build'))" $@
-
-echo 'Removing old build'
-rm -rf build
-rm -rf CMakeFiles
-rm -rf CMakeCache.txt
-rm -rf cmake_install.cmake
-
-mkdir build
-
-#sudo apt-get install libhdf5-serial-1.8.4
-#libhdf5-openmpi-dev
+mkdir -p build
 
 cd build
-
-#sudo apt-get install libcr-dev mpich2 mpich2-doc
 
 # Grab correct python executable
 export PYEXE=$(which python2.7)
@@ -37,7 +20,6 @@ fi
 echo "PYEXE              = $PYEXE"
 echo "PYTHON_EXECUTABLE  = $PYTHON_EXECUTABLE"
 echo "LOCAL_PREFIX       = $LOCAL_PREFIX"
-echo "INSTALL_CMD        = $INSTALL_CMD"
 echo "_SUDO              = $_SUDO"
 
 # Configure make build install
@@ -46,62 +28,32 @@ cmake -G "Unix Makefiles" \
     -DPYTHON_EXECUTABLE=$PYTHON_EXECUTABLE \
     -DBUILD_PYTHON_BINDINGS=On \
     -DBUILD_MATLAB_BINDINGS=Off \
-    -DBUILD_CUDA_LIB=Off\
-    -DCMAKE_INSTALL_PREFIX=$LOCAL_PREFIX\
+    -DBUILD_CUDA_LIB=Off \
+    -DBUILD_EXAMPLES=Off \
+    -DCMAKE_INSTALL_PREFIX=$LOCAL_PREFIX \
     ..
-
-    #-DNVCC_COMPILER_BINDIR=/usr/bin/gcc \
-    #-DCUDA_BUILD_CUBIN=On \
-    #-DCUDA_npp_LIBRARY=/usr/local/cuda-6.0/lib64/libnppc.so \
-    #-DHDF5_DIR=/home/joncrall/usr \
-    #-DHDF5_C_INCLUDE_DIR=/home/joncrall/usr/include \
-
-    #-DCMAKE_VERBOSE_MAKEFILE=On\
-    #-DCUDA_VERBOSE_BUILD=On\
-    #-DCUDA_NVCC_FLAGS=-gencode;arch=compute_20,code=sm_20;-gencode;arch=compute_20,code=sm_21
 
 make -j$NCPUS
 $_SUDO make install
-#make -j$NCPUS || { echo "FAILED MAKE" ; exit 1; }
-
-#sudo make install || { echo "FAILED MAKE INSTALL" ; exit 1; }
-
-# setup to develop (need to be in python source dir, setup is in build)
-# FIXME: messes up the code to find the libflann.so file when using build27
-#cd ../src/python
-#$_SUDO python ../../build/src/python/setup.py develop
 
 # Develop pyflann
 cd $FLANNDIR/src/python
 $_SUDO python ../../build/src/python/setup.py develop
 
-# NODE to use utprof.py you need to have flann sudo installed
-
 cd $FLANNDIR
-#cd $ORIGDIR
+
+echo << EOF
+GOALDS
+    * We want to be able to `pip install -e .` the flann/src/python directory
+    * We need the setup.py file to belong to there
+
+QUESTIONS:
+    * How should pip install -e . 
+    * How does the source know which build to use?
+EOF
 
 python -c "import pyflann; print(pyflann.__file__)"
 python -c "import pyflann; print(pyflann)"
-
-#copying pyflann/__init__.py -> build/lib.linux-x86_64-2.7/pyflann
-#copying pyflann/flann_ctypes.py -> build/lib.linux-x86_64-2.7/pyflann
-#copying pyflann/index.py -> build/lib.linux-x86_64-2.7/pyflann
-#copying pyflann/exceptions.py -> build/lib.linux-x86_64-2.7/pyflann
-#creating build/lib.linux-x86_64-2.7/pyflann/lib
-#copying /home/joncrall/tmp/flann/build/lib/libflann.so -> build/lib.linux-x86_64-2.7/pyflann/lib
-#package init file '/home/joncrall/tmp/flann/build/lib/__init__.py' not found (or not a regular file)
-#running install_lib
-#creating /home/joncrall/venv/lib/python2.7/site-packages/pyflann
-#copying build/lib.linux-x86_64-2.7/pyflann/__init__.py -> /home/joncrall/venv/lib/python2.7/site-packages/pyflann
-#copying build/lib.linux-x86_64-2.7/pyflann/flann_ctypes.py -> /home/joncrall/venv/lib/python2.7/site-packages/pyflann
-#copying build/lib.linux-x86_64-2.7/pyflann/index.py -> /home/joncrall/venv/lib/python2.7/site-packages/pyflann
-#creating /home/joncrall/venv/lib/python2.7/site-packages/pyflann/lib
-#copying build/lib.linux-x86_64-2.7/pyflann/lib/libflann.so -> /home/joncrall/venv/lib/python2.7/site-packages/pyflann/lib
-#copying build/lib.linux-x86_64-2.7/pyflann/exceptions.py -> /home/joncrall/venv/lib/python2.7/site-packages/pyflann
-#byte-compiling /home/joncrall/venv/lib/python2.7/site-packages/pyflann/__init__.py to __init__.pyc
-#byte-compiling /home/joncrall/venv/lib/python2.7/site-packages/pyflann/flann_ctypes.py to flann_ctypes.pyc
-#byte-compiling /home/joncrall/venv/lib/python2.7/site-packages/pyflann/index.py to index.pyc
-#byte-compiling /home/joncrall/venv/lib/python2.7/site-packages/pyflann/exceptions.py to exceptions.pyc
 
 flann_setuptools_install()
 {
@@ -113,14 +65,6 @@ flann_setuptools_install()
     python ../../build/src/python/setup.py develop --uninstall
     sudo python ../../build/src/python/setup.py develop --uninstall
 }
-#setupinstall_flann()
-#{
-#    code
-#    cd flann
-#    cd src/python
-#    python ../../build/src/python/setup.py install
-#}
-
 
 uninstall_flann()
 {
@@ -141,51 +85,115 @@ uninstall_flann()
     # Seems to work here: 880433b352d190fcbef78ea95d94ec8324059424
     # Seems to fail here: e5b9cbeabc9f790e231fbb91376a6842207565ba
 }
+#setupinstall_flann()
+#{
+#    code
+#    cd flann
+#    cd src/python
+#    python ../../build/src/python/setup.py install
+#}
 
 
-debug_flann()
+chaff()
 {
-    wget http://svn.python.org/projects/python/trunk/Misc/valgrind-python.supp
 
-    sh -c 'cat >> valgrind-python.supp << EOL
-{
-   ADDRESS_IN_RANGE/Invalid read of size 4
-   Memcheck:Addr4
-   fun:PyObject_Free
+#python -c "import utool as ut; print('keeping build dir' if not ut.get_argflag('--rmbuild') else ut.delete('build'))" $@
+#echo 'Removing old build'
+#rm -rf build
+#rm -rf CMakeFiles
+#rm -rf CMakeCache.txt
+#rm -rf cmake_install.cmake
+    #-DNVCC_COMPILER_BINDIR=/usr/bin/gcc \
+    #-DCUDA_BUILD_CUBIN=On \
+    #-DCUDA_npp_LIBRARY=/usr/local/cuda-6.0/lib64/libnppc.so \
+    #-DHDF5_DIR=/home/joncrall/usr \
+    #-DHDF5_C_INCLUDE_DIR=/home/joncrall/usr/include \
+
+    #-DCMAKE_VERBOSE_MAKEFILE=On\
+    #-DCUDA_VERBOSE_BUILD=On\
+    #-DCUDA_NVCC_FLAGS=-gencode;arch=compute_20,code=sm_20;-gencode;arch=compute_20,code=sm_21
+#cd $ORIGDIR
+#make -j$NCPUS || { echo "FAILED MAKE" ; exit 1; }
+
+#sudo make install || { echo "FAILED MAKE INSTALL" ; exit 1; }
+
+# setup to develop (need to be in python source dir, setup is in build)
+# FIXME: messes up the code to find the libflann.so file when using build27
+#cd ../src/python
+#$_SUDO python ../../build/src/python/setup.py develop
+# NODE to use utprof.py you need to have flann sudo installed
+#REPODIR=$(cd $(dirname $0) ; pwd)
+#cd $REPODIR/flann
+
+#rm -rf build
+#sudo apt-get install libhdf5-serial-1.8.4
+#libhdf5-openmpi-dev
+#sudo apt-get install libcr-dev mpich2 mpich2-doc
+#copying pyflann/__init__.py -> build/lib.linux-x86_64-2.7/pyflann
+#copying pyflann/flann_ctypes.py -> build/lib.linux-x86_64-2.7/pyflann
+#copying pyflann/index.py -> build/lib.linux-x86_64-2.7/pyflann
+#copying pyflann/exceptions.py -> build/lib.linux-x86_64-2.7/pyflann
+#creating build/lib.linux-x86_64-2.7/pyflann/lib
+#copying /home/joncrall/tmp/flann/build/lib/libflann.so -> build/lib.linux-x86_64-2.7/pyflann/lib
+#package init file '/home/joncrall/tmp/flann/build/lib/__init__.py' not found (or not a regular file)
+#running install_lib
+#creating /home/joncrall/venv/lib/python2.7/site-packages/pyflann
+#copying build/lib.linux-x86_64-2.7/pyflann/__init__.py -> /home/joncrall/venv/lib/python2.7/site-packages/pyflann
+#copying build/lib.linux-x86_64-2.7/pyflann/flann_ctypes.py -> /home/joncrall/venv/lib/python2.7/site-packages/pyflann
+#copying build/lib.linux-x86_64-2.7/pyflann/index.py -> /home/joncrall/venv/lib/python2.7/site-packages/pyflann
+#creating /home/joncrall/venv/lib/python2.7/site-packages/pyflann/lib
+#copying build/lib.linux-x86_64-2.7/pyflann/lib/libflann.so -> /home/joncrall/venv/lib/python2.7/site-packages/pyflann/lib
+#copying build/lib.linux-x86_64-2.7/pyflann/exceptions.py -> /home/joncrall/venv/lib/python2.7/site-packages/pyflann
+#byte-compiling /home/joncrall/venv/lib/python2.7/site-packages/pyflann/__init__.py to __init__.pyc
+#byte-compiling /home/joncrall/venv/lib/python2.7/site-packages/pyflann/flann_ctypes.py to flann_ctypes.pyc
+#byte-compiling /home/joncrall/venv/lib/python2.7/site-packages/pyflann/index.py to index.pyc
+#byte-compiling /home/joncrall/venv/lib/python2.7/site-packages/pyflann/exceptions.py to exceptions.pyc
 }
 
-{
-   ADDRESS_IN_RANGE/Invalid read of size 4
-   Memcheck:Value4
-   fun:PyObject_Free
-}
 
-{
-   ADDRESS_IN_RANGE/Conditional jump or move depends on uninitialised value
-   Memcheck:Cond
-   fun:PyObject_Free
-}
+#debug_flann()
+#{
+#    wget http://svn.python.org/projects/python/trunk/Misc/valgrind-python.supp
 
-{
-   ADDRESS_IN_RANGE/Invalid read of size 4
-   Memcheck:Addr4
-   fun:PyObject_Realloc
-}
+#    sh -c 'cat >> valgrind-python.supp << EOL
+#{
+#   ADDRESS_IN_RANGE/Invalid read of size 4
+#   Memcheck:Addr4
+#   fun:PyObject_Free
+#}
 
-{
-   ADDRESS_IN_RANGE/Invalid read of size 4
-   Memcheck:Value4
-   fun:PyObject_Realloc
-}
+#{
+#   ADDRESS_IN_RANGE/Invalid read of size 4
+#   Memcheck:Value4
+#   fun:PyObject_Free
+#}
 
-{
-   ADDRESS_IN_RANGE/Conditional jump or move depends on uninitialised value
-   Memcheck:Cond
-   fun:PyObject_Realloc
-}
-EOL'
-    valgrind --tool=memcheck --suppressions=valgrind-python.supp python -E -tt ./examples/example.py
+#{
+#   ADDRESS_IN_RANGE/Conditional jump or move depends on uninitialised value
+#   Memcheck:Cond
+#   fun:PyObject_Free
+#}
 
-    valgrind --tool=memcheck --suppressions=valgrind-python.supp python  ./examples/example.py
+#{
+#   ADDRESS_IN_RANGE/Invalid read of size 4
+#   Memcheck:Addr4
+#   fun:PyObject_Realloc
+#}
 
-}
+#{
+#   ADDRESS_IN_RANGE/Invalid read of size 4
+#   Memcheck:Value4
+#   fun:PyObject_Realloc
+#}
+
+#{
+#   ADDRESS_IN_RANGE/Conditional jump or move depends on uninitialised value
+#   Memcheck:Cond
+#   fun:PyObject_Realloc
+#}
+#EOL'
+#    valgrind --tool=memcheck --suppressions=valgrind-python.supp python -E -tt ./examples/example.py
+
+#    valgrind --tool=memcheck --suppressions=valgrind-python.supp python  ./examples/example.py
+
+#}
