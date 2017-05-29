cee686dde26a78cbb09cba7b40df65ad34e41336
joncrall <crallj@rpi.edu>
2017-05-29 18:19:11 -0400

    wip

---
+++ test/simple_benchmark.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import pyflann
+import numpy as np
+
+
+def rand_vecs(num, dim, rng=np.random, dtype=np.uint8):
+    return (rng.rand(num, dim) * 255).astype(dtype)
+
+
+def run_benchmark():
+    import ubelt as ub
+    data_dim = 128
+    num_dpts = 1000000
+    num_qpts = 10000
+    num_neighbs = 5
+    random_seed = 42
+    rng = np.random.RandomState(0)
+
+    dataset = rand_vecs(num_dpts, data_dim, rng)
+    testset = rand_vecs(num_qpts, data_dim, rng)
+    # Build determenistic flann object
+    flann = pyflann.FLANN()
+    print('building datset for %d vecs' % (len(dataset)))
+
+    with ub.Timer(label='building kdtrees', verbose=True) as t:
+        flann.build_index(dataset, algorithm='kdtree', trees=4,
+                          random_seed=random_seed)
+
+    qvec_chunks = list(ub.chunks(testset, 1000))
+    times = []
+    for qvecs in ub.ProgIter(qvec_chunks, label='find nn'):
+        with ub.Timer(verbose=0) as t:
+            _ = flann.nn_index(testset, num_neighbs)  # NOQA
+        times.append(t.ellapsed)
+
+if __name__ == '__main__':
+    run_benchmark()
