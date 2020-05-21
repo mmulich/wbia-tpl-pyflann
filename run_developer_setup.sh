e57027fa12c41526c79a6398238a9a8944c66020
Jason Parham <bluemellophone@gmail.com>
2020-05-21 15:55:19 -0700

    Added support for MacOS build

--- run_developer_setup.sh
+++ run_developer_setup.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 __heredoc__="""
-sudo apt-get install libhdf5-serial-dev
-sudo apt install liblz4-dev
+# Ubuntu
+sudo apt install liblz4-dev libhdf5-serial-dev
+
+# MacOS with MacPorts
+sudo port install ninja hdf5 lz4 hdf5-lz4-plugin
+pip install -U scikit-build cmake lz4
 """
 
 python setup.py build_ext --inplace
