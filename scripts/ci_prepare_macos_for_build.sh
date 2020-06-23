53fd626a7a674c66eca935a98c855fcec104bbdb
Michael Mulich <michael.mulich@gmail.com>
2020-06-23 10:38:12 -0700

    Add lz4 macos dependency

--- scripts/ci_prepare_macos_for_build.sh
+++ scripts/ci_prepare_macos_for_build.sh
@@ -5,5 +5,11 @@ set -ex
 # See https://stackoverflow.com/a/246128/176882
 export CUR_LOC="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
+
+# Install system dependencies
+brew install lz4
+
+
 # Install Python package dependencies
 python -m pip install -r requirements/build.txt
+
