2424931443f8652671959bf4206c3b5f4dbdc660
Michael Mulich <michael.mulich@gmail.com>
2020-06-23 08:31:58 -0700

    Workflow for publishing python wheels

---
+++ scripts/ci_prepare_macos_for_build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -ex
+
+# See https://stackoverflow.com/a/246128/176882
+export CUR_LOC="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+# Install Python package dependencies
+python -m pip install -r requirements/build.txt
